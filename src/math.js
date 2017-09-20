75c9e9f40cbcb981f34c9c0ea4e54e3a45620f1c
Ryan Zander <exide@hotmail.com>
2017-09-20 12:21:15 -0700

    wip; Moving game logic to the server and synchronizing using WebSockets.

--- src/math.js
+++
@@ -1,8 +0,0 @@
-export const RelativeDirection = Object.freeze({
-  Left: Symbol.for('Left'),
-  Right: Symbol.for('Right'),
-  Up: Symbol.for('Up'),
-  Down: Symbol.for('Down'),
-  Forward: Symbol.for('Forward'),
-  Backward: Symbol.for('Backward')
-});
